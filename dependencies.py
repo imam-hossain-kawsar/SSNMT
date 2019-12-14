754f33ccd045b92e4244ca2684c6116b3da34e4a
imam-hossain-kawsar <32457434+imam-hossain-kawsar@users.noreply.github.com>
2019-12-14 20:19:55 +0600

    dependencies installer

---
+++ dependencies.py
@@ -0,0 +1,10 @@
+import subprocess
+# subprocess.call('sudo apt update', shell=True)
+# subprocess.call('sudo apt install python3-pip', shell=True)
+# subprocess.call('sudo pip install supyr-struct', shell=True)
+#subprocess.call('sudo apt-get install python-tk', shell=True)
+
+
+
+
+
